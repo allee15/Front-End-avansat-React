f524e5eaa6a19ce155888f3a05037cecbbf95f65
Alexia Aldea <alexia.elena.aldea@gmail.com>
2023-10-12 21:49:26 +0300

    added contact us form

--- src/components/Counter.js
+++
@@ -1,9 +0,0 @@
-
-const Counter = (props) => {
-    return <div>
-        <button onClick={() => props.setCounter(props.counter+1)}>Increment</button>
-        <div>{props.counter}</div>
-    </div>
-}
-
-export default Counter;
--- src/pages/ContactUsPage/index.js
+++ src/pages/ContactUsPage/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function ContactUsPage() {
+export default function ContactUsPage() {
+    const [message, setMessage] = useState()
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_e7nt70t', 'template_c10k7xl', e.target, 'gJcEBYj0HnQj8Vf1Y')
+            .then((result) => {
+                setMessage("");
+                toast.success("Message successfully sent!");
+            }, (error) => {
+                toast.error("An error has occured. Try again!");
+            });
+    };
+
     return (
         <div>
             <h1>Contact Us</h1>
+            <h2>If you're interested in adopting one of our pets, write us here its name, how can we reach out to you and we will come as soon as possible with a response to you!</h2>
+            <form onSubmit={sendEmail}>
+                <textarea style={{width: '100%', height: '200px', paddingLeft: '20px', paddingRight: '20px'}} value={message} onChange={e => setMessage(e.target.value)} name="message"></textarea>
+                <button style={{backgroundColor: 'darkslategrey', color: 'white', width: '100%', height: '50px'}} type="submit">Send</button>
+            </form>
+            <ToastContainer />
         </div>
     );
 }
 
-export default ContactUsPage;
+
+
